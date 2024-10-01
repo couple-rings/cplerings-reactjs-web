875f7b18c0ced906bc6920520a255f9ea5c6cc0b
Nguyen Quoc Tin <tin.9a1.dt@gmail.com>
2024-10-01 15:55:24 +0700

    WEB-2.3: Completed

---
+++ src/pages/Common/ResetPassword/ResetPassword.tsx
@@ -0,0 +1,132 @@
+import {
+  Button,
+  FormControl,
+  FormHelperText,
+  Grid,
+  IconButton,
+  InputAdornment,
+  OutlinedInput,
+} from "@mui/material";
+import styles from "./ResetPassword.module.scss";
+import { primaryBtn } from "src/utils/styles";
+import { useState } from "react";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { passwordPattern } from "src/utils/constants";
+
+interface IFormInput {
+  password: string;
+  cfnPassword: string;
+  otp: string;
+}
+
+const ResetPassword = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showCfnPassword, setShowCfnPassword] = useState(false);
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    getValues,
+  } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+  return (
+    <div className={styles.container}>
+      <Grid container sx={{ justifyContent: "center" }}>
+        <Grid item sm={8} md={5} lg={3}>
+          <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+            <div className={styles.title}>Đổi Mật Khẩu</div>
+
+            <FormControl variant="outlined">
+              <OutlinedInput
+                sx={{ borderRadius: 0 }}
+                placeholder="Mật khẩu mới"
+                type={showPassword ? "text" : "password"}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={() => setShowPassword((show) => !show)}
+                      edge="start"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                error={!!errors.password}
+                {...register("password", {
+                  required: "Vui lòng nhập mật khẩu",
+                  pattern: {
+                    value: passwordPattern,
+                    message:
+                      "Tối thiểu 8 ký tự bao gồm ký tự đặc biệt, số và chữ cái",
+                  },
+                })}
+              />
+              {errors.password && (
+                <FormHelperText error>{errors.password.message}</FormHelperText>
+              )}
+            </FormControl>
+
+            <FormControl variant="outlined">
+              <OutlinedInput
+                sx={{ borderRadius: 0 }}
+                placeholder="Nhập lại mật khẩu"
+                type={showCfnPassword ? "text" : "password"}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={() => setShowCfnPassword((show) => !show)}
+                      edge="start"
+                    >
+                      {showCfnPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                error={!!errors.cfnPassword}
+                {...register("cfnPassword", {
+                  required: "Vui lòng xác nhận mật khẩu",
+                  validate: (value) => {
+                    const { password } = getValues();
+                    if (value === password) {
+                      return true;
+                    } else {
+                      return "Mật khẩu xác nhận không chính xác";
+                    }
+                  },
+                })}
+              />
+              {errors.cfnPassword && (
+                <FormHelperText error>
+                  {errors.cfnPassword.message}
+                </FormHelperText>
+              )}
+            </FormControl>
+
+            <FormControl variant="outlined">
+              <OutlinedInput
+                sx={{ borderRadius: 0 }}
+                placeholder="Nhập mã OTP"
+                error={!!errors.otp}
+                {...register("otp", {
+                  required: "Vui lòng nhập mã OTP",
+                })}
+              />
+              {errors.otp && (
+                <FormHelperText error>{errors.otp.message}</FormHelperText>
+              )}
+            </FormControl>
+
+            <Button variant="contained" sx={primaryBtn} fullWidth type="submit">
+              Xác Nhận
+            </Button>
+          </form>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default ResetPassword;
