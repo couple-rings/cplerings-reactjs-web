0d874cfbd7cc092f05b5080799ee7c631aa65f80
Bumbobin <ndthung281@gmail.com>
2024-10-02 11:47:12 +0700

    Update register screen

--- src/pages/Common/Register/Register.tsx
+++ src/pages/Common/Register/Register.tsx
@@ -2,23 +2,37 @@ import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import styles from "./Register.module.scss";
 import { useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { Button, FormControl, FormHelperText, Grid, TextField } from "@mui/material";
+import {
+  Button,
+  FormControl,
+  FormHelperText,
+  Grid,
+  IconButton,
+  InputAdornment,
+  TextField,
+} from "@mui/material";
 import { emailPattern, passwordPattern } from "src/utils/constants";
 import { primaryBtn } from "src/utils/styles";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import Visibility from "@mui/icons-material/Visibility";
 
 interface IFormInput {
   email: string;
   password: string;
+  confirmPassword: string;
   code: string;
 }
 
 const Register = () => {
   const [code, setCode] = useState(0);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   const {
     register,
     formState: { errors },
     handleSubmit,
+    watch,
   } = useForm<IFormInput>();
   const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
 
@@ -36,7 +50,7 @@ const Register = () => {
 
             <FormControl fullWidth variant="outlined">
               <TextField
-              fullWidth
+                fullWidth
                 // className={styles.type}
                 id="outlined-basic"
                 label="Email"
@@ -57,17 +71,40 @@ const Register = () => {
 
             <FormControl fullWidth>
               <TextField
-              fullWidth
+                fullWidth
                 // className={styles.type}
-                type="password"
+                type={showPassword ? "text" : "password"}
                 id="outlined-basic"
                 label="Mật khẩu"
+                InputProps={{
+                  sx: {
+                    display: "flex",
+                    alignItems: "center",
+                    margin: "0",
+                  },
+                  endAdornment: (
+                    <InputAdornment position="end" sx={{ margin: "0" }}>
+                      <IconButton
+                        onClick={() => setShowPassword((show) => !show)}
+                        edge="start"
+                        sx={{
+                          margin: "0!important",
+                          backgroundColor: "white!important",
+                          color: "black!important",
+                        }} // Ensure no margin and padding on IconButton
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
                 error={!!errors.password}
                 {...register("password", {
                   required: "* Vui lòng nhập mật khẩu",
                   pattern: {
                     value: passwordPattern,
-                    message: "* Mật khẩu không hợp lệ",
+                    message:
+                      "* Tối thiểu 8 ký tự bao gồm ký tự đặc biệt, số và chữ cái",
                   },
                 })}
                 variant="outlined"
@@ -77,50 +114,88 @@ const Register = () => {
               )}
             </FormControl>
 
+            <FormControl fullWidth>
+              <TextField
+                fullWidth
+                // className={styles.type}
+                type={showConfirmPassword ? "text" : "password"}
+                id="outlined-basic"
+                label="Xác nhận mật khẩu"
+                InputProps={{
+                  sx: {
+                    display: "flex",
+                    alignItems: "center",
+                    margin: "0",
+                  },
+                  endAdornment: (
+                    <InputAdornment position="end" sx={{ margin: "0" }}>
+                      <IconButton
+                        onClick={() => setShowConfirmPassword((show) => !show)}
+                        edge="start"
+                        sx={{
+                          margin: "0!important",
+                          backgroundColor: "white!important",
+                          color: "black!important",
+                        }} // Ensure no margin and padding on IconButton
+                      >
+                        {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+                error={!!errors.confirmPassword}
+                {...register("confirmPassword", {
+                  required: "* Vui lòng nhập xác nhận mật khẩu",
+                  validate: (value) =>
+                    value === watch('password') || "* Mật khẩu xác nhận không khớp",
+                  
+                })}
+                variant="outlined"
+              />
+              {errors.confirmPassword && (
+                <FormHelperText error>{errors.confirmPassword.message}</FormHelperText>
+              )}
+            </FormControl>
+
             <div className={styles.codeContainer}>
-  <FormControl variant="outlined" sx={{ flex: 1 }} fullWidth>
-    <TextField
-      fullWidth
-      id="outlined-basic"
-      label="Code"
-      error={!!errors.code}
-      {...register("code", {
-        required: "* Vui lòng nhập code",
-        validate: (value) => {
-          if (+value === code) {
-            return true;
-          } else {
-            return "Code không hợp lệ";
-          }
-        },
-      })}
-      variant="outlined"
-    />
-    {errors.code && (
-      <FormHelperText error>{errors.code.message}</FormHelperText>
-    )}
-  </FormControl>
+              <FormControl variant="outlined" sx={{ flex: 1 }} fullWidth>
+                <TextField
+                  fullWidth
+                  id="outlined-basic"
+                  label="Code"
+                  error={!!errors.code}
+                  {...register("code", {
+                    required: "* Vui lòng nhập code",
+                    validate: (value) => {
+                      if (+value === code) {
+                        return true;
+                      } else {
+                        return "Code không hợp lệ";
+                      }
+                    },
+                  })}
+                  variant="outlined"
+                />
+                {errors.code && (
+                  <FormHelperText error>{errors.code.message}</FormHelperText>
+                )}
+              </FormControl>
 
-  <div className={styles.code}>{code}</div>
-</div>
-
-
-            
-
-            
+              <div className={styles.code}>{code}</div>
+            </div>
 
             <Button variant="contained" sx={primaryBtn} fullWidth type="submit">
               Tạo tài khoản
             </Button>
 
             <p className={styles.description}>
-              By creating this account, you agree to the DR’s user registration
-              agreement and privacy terms
+              Bằng cách tạo tài khoản này, bạn đồng ý với thỏa thuận đăng ký
+              người dùng và các điều khoản về quyền riêng tư của CR
             </p>
 
-            <h2 className="note">Already have one</h2>
+            <h2 className="note">Đã có tài khoản</h2>
             <p className={styles.navigateSignIn}>
-              Sign in{" "}
+              Đăng nhập{" "}
               <NavigateNextIcon
                 sx={{
                   fontSize: 20,
