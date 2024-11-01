0ae9d60fd4e28036361524f5cf99d8cf28cbecba
Bumbobin <ndthung281@gmail.com>
2024-11-01 14:44:49 +0700

    View custom order detail

--- src/pages/Jeweler/OrderCustomDetail/OrderCustomDetail.tsx
+++ src/pages/Jeweler/OrderCustomDetail/OrderCustomDetail.tsx
@@ -1,5 +1,6 @@
-import {  Grid } from "@mui/material";
-import Divider from '@mui/material/Divider';
+import { Button, Grid } from "@mui/material";
+import Divider from "@mui/material/Divider";
+
 import blueprint from "src/assets/sampledata/blueprint.pdf";
 import menring from "src/assets/sampledata/menring.png";
 import HoverCard from "src/components/product/HoverCard";
@@ -18,22 +19,38 @@ function OrderCustomDetail() {
         DR FOREVER Two-row Diamond Pavé Wedding Rings
       </div>
 
-      <Grid container className="container-biggest">
-        <Grid container item md={5} justifyContent={"center"} className="container-item">
+      <Grid
+        container
+        justifyContent={"space-around"}
+        className="container-biggest"
+      >
+        <Grid
+          container
+          item
+          md={4}
+          justifyContent={"center"}
+          className="container-item"
+          pb={5}
+        >
           <Grid item xs={12} md={12} pb={3} className="image-item">
             <HoverCard file={blueprint} image={menring} />
           </Grid>
 
           <Grid container pb={5} className={styles.genderDefine}>
-            <Grid item  className="gender-icon">
-              <MaleIcon />
+            <Grid item className="gender-icon">
+              <MaleIcon sx={{ color: "#4994ec" }} />
             </Grid>
             <Grid item className="gender-name">
               Nam giới
             </Grid>
           </Grid>
 
-          <Grid container item justifyContent={"center"} className="info-detail-container">
+          <Grid
+            container
+            item
+            justifyContent={"center"}
+            className="info-detail-container"
+          >
             <Grid container className={styles.infoDetail}>
               <Grid item className="info-detail-label">
                 Chất Liệu
@@ -43,7 +60,9 @@ function OrderCustomDetail() {
               </Grid>
             </Grid>
 
-            <Divider/>
+            <Grid item xs={12}>
+              <Divider />
+            </Grid>
 
             <Grid container className={styles.infoDetail}>
               <Grid item className="info-detail-label">
@@ -52,6 +71,10 @@ function OrderCustomDetail() {
               <Grid item className="info-detail-content">
                 12PT, D
               </Grid>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Divider />
             </Grid>
 
             <Grid container className={styles.infoDetail}>
@@ -63,6 +86,10 @@ function OrderCustomDetail() {
               </Grid>
             </Grid>
 
+            <Grid item xs={12}>
+              <Divider />
+            </Grid>
+
             <Grid container className={styles.infoDetail}>
               <Grid item className="info-detail-label">
                 Khắc Chữ
@@ -70,6 +97,10 @@ function OrderCustomDetail() {
               <Grid item className="info-detail-content">
                 Hehehoho
               </Grid>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Divider />
             </Grid>
 
             <Grid container className={styles.infoDetail}>
@@ -81,6 +112,10 @@ function OrderCustomDetail() {
               </Grid>
             </Grid>
 
+            <Grid item xs={12}>
+              <Divider />
+            </Grid>
+
             <Grid container className={styles.infoDetail}>
               <Grid item className="info-detail-label">
                 Ghi chú
@@ -88,6 +123,10 @@ function OrderCustomDetail() {
               <Grid item className="info-detail-content">
                 Hong có gì hết chơn
               </Grid>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Divider />
             </Grid>
 
             <Grid container className={styles.infoDetail}>
@@ -98,10 +137,128 @@ function OrderCustomDetail() {
                 1,000,000VND
               </Grid>
             </Grid>
-
-
           </Grid>
         </Grid>
+
+        <Grid
+          container
+          item
+          md={4}
+          justifyContent={"center"}
+          className="container-item"
+          pb={5}
+        >
+          <Grid item xs={12} md={12} pb={3} className="image-item">
+            <HoverCard file={blueprint} image={menring} />
+          </Grid>
+
+          <Grid container pb={5} className={styles.genderDefine}>
+            <Grid item className="gender-icon">
+              <MaleIcon sx={{ color: "#ea394b" }} />
+            </Grid>
+            <Grid item className="gender-name">
+              Nữ giới
+            </Grid>
+          </Grid>
+
+          <Grid
+            container
+            item
+            justifyContent={"center"}
+            className="info-detail-container"
+          >
+            <Grid container className={styles.infoDetail}>
+              <Grid item className="info-detail-label">
+                Chất Liệu
+              </Grid>
+              <Grid item className="info-detail-content">
+                18K white gold
+              </Grid>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Divider />
+            </Grid>
+
+            <Grid container className={styles.infoDetail}>
+              <Grid item className="info-detail-label">
+                Kim cương
+              </Grid>
+              <Grid item className="info-detail-content">
+                12PT, D
+              </Grid>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Divider />
+            </Grid>
+
+            <Grid container className={styles.infoDetail}>
+              <Grid item className="info-detail-label">
+                Kích cỡ
+              </Grid>
+              <Grid item className="info-detail-content">
+                3
+              </Grid>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Divider />
+            </Grid>
+
+            <Grid container className={styles.infoDetail}>
+              <Grid item className="info-detail-label">
+                Khắc Chữ
+              </Grid>
+              <Grid item className="info-detail-content">
+                Hehehoho
+              </Grid>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Divider />
+            </Grid>
+
+            <Grid container className={styles.infoDetail}>
+              <Grid item className="info-detail-label">
+                Trọng Lượng
+              </Grid>
+              <Grid item className="info-detail-content">
+                5 chỉ
+              </Grid>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Divider />
+            </Grid>
+
+            <Grid container className={styles.infoDetail}>
+              <Grid item className="info-detail-label">
+                Ghi chú
+              </Grid>
+              <Grid item className="info-detail-content">
+                Hong có gì hết chơn
+              </Grid>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Divider />
+            </Grid>
+
+            <Grid container className={styles.infoDetail}>
+              <Grid item className="info-detail-label">
+                Giá
+              </Grid>
+              <Grid item className="info-detail-content">
+                1,000,000VND
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid container justifyContent={"center"}>
+        <Button>Nhận làm bản thiết kế này</Button>
       </Grid>
     </div>
   );
