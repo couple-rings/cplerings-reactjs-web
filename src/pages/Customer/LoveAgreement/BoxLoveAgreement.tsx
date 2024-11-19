9f9d60f7dcb1786b6c832af9e17c60d36c05bf43
Bumbobin <ndthung281@gmail.com>
2024-11-19 07:40:18 +0700

    Fix bug SCSS

--- src/pages/Customer/LoveAgreement/BoxLoveAgreement.tsx
+++ src/pages/Customer/LoveAgreement/BoxLoveAgreement.tsx
@@ -1,7 +1,7 @@
 
 import { Box } from "@mui/material";
 import thumbImg from "../../../assets/agreementPic.png";
-import styles, { signDate } from "./BoxLoveAgreement.module.scss";
+import styles from "./BoxLoveAgreement.module.scss";
 
 function BoxLoveAgreement(props: ILoveAgreement) {
     const {spouseName, date} = props;
@@ -10,7 +10,7 @@ function BoxLoveAgreement(props: ILoveAgreement) {
             <img src={thumbImg} alt="" />
             <div className={styles.boxContent}>
                 <div className={styles.ownName}>{spouseName}</div>
-                <div className={signDate}>{date}</div>
+                <div className={styles.signDate}>{date}</div>
             </div>
         </Box>
     )
